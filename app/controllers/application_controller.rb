acb841488d8a01cd75013c08f8d090761f666377
runrunryosuke <runrunryosuke0425@gmail.com>
2020-09-09 22:42:03 +0900

    udemy

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  before_action :current_user
+
+  private
+    def current_user
+      return unless session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+    end
+end
